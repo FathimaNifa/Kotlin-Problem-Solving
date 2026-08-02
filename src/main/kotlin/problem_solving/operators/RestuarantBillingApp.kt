ebc0e329fbbfd753e552d306afcf5e28ba632737
FathimaNifa <fathimamk226@gmail.com>
2026-08-02 19:23:22 +0530

    Comments addressed

--- src/main/kotlin/problem_solving/operators/RestuarantBillingApp.kt
+++ src/main/kotlin/problem_solving/operators/RestuarantBillingApp.kt
@@ -4,15 +4,16 @@ fun main() {
     getCustomerDetails()
 
 }
-fun getCustomerDetails(){
+
+fun getCustomerDetails() {
     print("Enter Customer Name        : ")
     val customerName = readln()
     print("Enter Item1 Price          : ")
     val item1Price = readln().toInt()
     print("Enter Item1 Quantity       : ")
-    val item1Quantity  = readln().toInt()
+    val item1Quantity = readln().toInt()
     print("Enter Item2 Price          : ")
-    val item2Price  = readln().toInt()
+    val item2Price = readln().toInt()
     print("Enter Item2 Quantity       : ")
     val item2Quantity = readln().toInt()
     print("Enter Item3 Price          : ")
@@ -23,19 +24,53 @@ fun getCustomerDetails(){
     val gstPercent = readln().toInt()
     print("Enter Discount Percentage  : ")
     val disPercent = readln().toInt()
-    val item1 = calculateItem1(item1Price,item1Quantity  )
-    val item2 = calculateItem2(item2Price,item2Quantity  )
-    val item3 = calculateItem3(item3Price,item3Quantity  )
+    val item1 = priceCalculation(item1Price, item1Quantity)
+    val item2 = priceCalculation(item2Price, item2Quantity)
+    val item3 = priceCalculation(item3Price, item3Quantity)
     val subTotal = calculateSubtotal(item1, item2, item3)
     val gst = calculateGST(subTotal, gstPercent)
-    val discount = calculateDiscount(subTotal,disPercent)
+    val discount = calculateDiscount(subTotal, disPercent)
     val finalAmount = calculateFinalAmount(subTotal, gst, discount)
-    printRestaurantBill(customerName, item1, item2, item3, subTotal, gst, discount, finalAmount, gstPercent,disPercent, item1Price, item2Price, item3Price, item1Quantity, item2Quantity, item3Quantity)
+    printRestaurantBill(
+        customerName,
+        item1,
+        item2,
+        item3,
+        subTotal,
+        gst,
+        discount,
+        finalAmount,
+        gstPercent,
+        disPercent,
+        item1Price,
+        item2Price,
+        item3Price,
+        item1Quantity,
+        item2Quantity,
+        item3Quantity
+    )
 
 }
 
 
-fun printRestaurantBill(customerName : String, item1 : Int, item2 : Int, item3 : Int, subTotal: Int, gst : Int, discount: Int, finalAmount : Int, gstPercent: Int, disPercent: Int,item1Price: Int,item2Price: Int,item3Price: Int,item1Quantity: Int, item2Quantity: Int,item3Quantity: Int) {
+fun printRestaurantBill(
+    customerName: String,
+    item1: Int,
+    item2: Int,
+    item3: Int,
+    subTotal: Int,
+    gst: Int,
+    discount: Int,
+    finalAmount: Int,
+    gstPercent: Int,
+    disPercent: Int,
+    item1Price: Int,
+    item2Price: Int,
+    item3Price: Int,
+    item1Quantity: Int,
+    item2Quantity: Int,
+    item3Quantity: Int
+) {
     println("========================================")
     println("          RESTAURANT BILL")
     println("========================================")
@@ -57,25 +92,19 @@ fun printRestaurantBill(customerName : String, item1 : Int, item2 : Int, item3 :
     println("        THANK YOU! VISIT AGAIN...")
     println("========================================")
 }
-fun calculateItem1(item1Price: Int, item1Quantity: Int ): Int = item1Price * item1Quantity
+
+fun priceCalculation(itemPrice: Int, itemQuality: Int) = itemPrice * itemQuality
 
 
-
-fun calculateItem2(item2Price: Int, item2Quantity: Int ): Int = item2Price * item2Quantity
-
-
-fun calculateItem3(item3Price: Int, item3Quantity: Int ): Int = item3Price * item3Quantity
-
-fun calculateSubtotal(item1: Int, item2 : Int, item3 : Int): Int = item1+item2+item3
+fun calculateSubtotal(item1: Int, item2: Int, item3: Int): Int = item1 + item2 + item3
 
 
+fun calculateGST(subTotal: Int, gstPercent: Int): Int = (subTotal * gstPercent) / 100
 
-fun calculateGST(subTotal: Int, gstPercent: Int): Int = (subTotal*gstPercent)/100
 
+fun calculateDiscount(subTotal: Int, disPercent: Int): Int = (subTotal * disPercent) / 100
 
-fun calculateDiscount(subTotal: Int, disPercent: Int): Int = (subTotal*disPercent)/100
-
-fun calculateFinalAmount(subTotal: Int, gst: Int, discount: Int): Int = subTotal+gst-discount
+fun calculateFinalAmount(subTotal: Int, gst: Int, discount: Int): Int = subTotal + gst - discount
 
 
 //## Problem Statement
