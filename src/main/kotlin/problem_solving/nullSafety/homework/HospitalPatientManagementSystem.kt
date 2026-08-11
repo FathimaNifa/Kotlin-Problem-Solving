77ba12eb65ee199e98fb87cc1545762a4949679f
FathimaNifa <fathimamk226@gmail.com>
2026-08-11 11:37:14 +0530

    Null Safety - Home work - Completed

--- src/main/kotlin/problem_solving/nullSafety/homework/HospitalPatientManagementSystem.kt
+++ src/main/kotlin/problem_solving/nullSafety/homework/HospitalPatientManagementSystem.kt
@@ -11,101 +11,141 @@ class Patient {
     var bloodGroup: String? = ""
     var contactNumber: String? = ""
 
+
     fun displayPatientDetails() {
         println("===========Patient Detail==========")
         println("Patient ID     : $patientID")
         println("Patient Name   : ${patientName ?: "Not Available"}")
         println("Doctor Name    : ${doctorName ?: "Not Available"}")
-        println("Blood Group    : ₹$bloodGroup")
+        println("Blood Group    : ${bloodGroup ?: "Not Available"}")
         println("Contact Number : ${contactNumber ?: "Not Available"}")
         println()
     }
 
-    fun updateDoctor(): String{
+    fun getUpdateDoctorInput(): UserInput {
+        val userInput = UserInput()
         print("Enter Patient ID : ")
-        val getPatientID = readln().toString()
-        patientID=getPatientID
-        println("Enter Doctor Name : ")
-       val updateDoctorName= readln().toString()
+        userInput.patientId = readln().toString()
+        print("Enter Doctor Name : ")
+        userInput.updateDoctorName = readln().toString()
+        return userInput
+    }
+    fun getUpdateContactInput(): UserInput{
+        val userInput = UserInput()
+        print("Enter Patient ID : ")
+        userInput.patientId = readln().toString()
+        print("Enter Contact Number : ")
+        userInput.updateContactNumber = readln().toString()
+        return userInput
+    }
+
+    fun updateDoctor(updateDoctorName: String) {
+
         doctorName=updateDoctorName
-        return doctorName as String
+        displayPatientDetails()
+
+    }
+
+    fun updateContactNumber(updateContactNumber : String) {
+
+        contactNumber=updateContactNumber
         displayPatientDetails()
     }
 
-    fun displayMenu() {
-        while (true) {
-            println("==================================")
-            println("    Hospital Management System ")
-            println("==================================")
-            println()
-            println("1. View All Patients")
-            println("2. Update Doctor")
-            println("3. Update Contact Number")
-            println("4. Exit")
-            println()
-            println("--------------------------")
-            println()
-            print("Enter your Choice: ")
-
-            val choice = readln().toInt()
-
-            when (choice) {
-                1 -> {
-
-                }
-
-                2 -> {
-                    updateDoctor()
-                }
-
-                3 -> {
-
-                }
-
-                4 -> {
-                    println("Thank you for using the ATM!")
-                    break
-                }
-
-                else -> {
-                    println("Invalid Input")
-                }
-            }
-            println()
-        }
-
-
+    class UserInput {
+        var patientId = ""
+        var updateDoctorName = ""
+        var updateContactNumber = ""
     }
 }
-
 fun main(){
-    val patient1 = Patient()
-    val patient2 = Patient()
-    val patient3 = Patient()
-
-    patient1.patientID="P101"
-    patient2.patientID="P102"
-    patient3.patientID="P103"
-
-    patient1.patientName = null
-    patient2.patientName = "Ravi"
-    patient3.patientName = "Ramya"
-
-    patient1.doctorName = "Dr.Hari"
-    patient2.doctorName = null
-    patient3.doctorName = "Dr.Renu"
-
-    patient1.bloodGroup = "A+ve"
-    patient2.bloodGroup = "B-ve"
-    patient3.bloodGroup = null
-
-    patient1.contactNumber = "9876543231"
-    patient2.contactNumber = null
-    patient3.contactNumber = "5684734765"
+    val patientList = arrayOf(Patient(),Patient(),Patient())
 
 
+    patientList[0].patientID="P101"
+    patientList[1].patientID="P102"
+    patientList[2].patientID="P103"
+
+    patientList[0].patientName = null
+    patientList[1].patientName = "Ravi"
+    patientList[2].patientName = "Ramya"
+
+    patientList[0].doctorName = "Dr.Hari"
+    patientList[1].doctorName = null
+    patientList[2].doctorName = "Dr.Renu"
+
+    patientList[0].bloodGroup = "A+ve"
+    patientList[1].bloodGroup = "B-ve"
+    patientList[2].bloodGroup = null
+
+    patientList[0].contactNumber = "9876543231"
+    patientList[1].contactNumber = null
+    patientList[2].contactNumber = "5684734765"
+
+    displayMenu(patientList[0],patientList[1],patientList[2])
 
 
 
+}
+
+fun displayMenu(patient1: Patient,patient2: Patient,patient3 :Patient) {
+    while (true) {
+        println("==================================")
+        println("    Hospital Management System ")
+        println("==================================")
+        println()
+        println("1. View All Patients")
+        println("2. Update Doctor")
+        println("3. Update Contact Number")
+        println("4. Exit")
+        println()
+        println("--------------------------")
+        println()
+        print("Enter your Choice: ")
+
+        val choice = readln().toInt()
+
+        when (choice) {
+            1 -> {
+                println("======================================")
+                patient1.displayPatientDetails()
+                println("======================================")
+                patient2.displayPatientDetails()
+                println("======================================")
+                patient3.displayPatientDetails()
+                println("======================================")
+            }
+
+            2 -> {
+                val userInput = patient1.getUpdateDoctorInput()
+                when (userInput.patientId) {
+                    "P101"-> patient1.updateDoctor(userInput.updateDoctorName)
+                    "P102" -> patient2.updateDoctor(userInput.updateDoctorName)
+                    "P103" -> patient3.updateDoctor(userInput.updateDoctorName)
+                    else -> println("Invalid Input")
+                }
+            }
+
+            3 -> {
+                val userInput = patient1.getUpdateContactInput()
+                when (userInput.patientId) {
+                    "P101" -> patient1.updateContactNumber(userInput.updateContactNumber)
+                    "P102" -> patient2.updateContactNumber(userInput.updateContactNumber)
+                    "P103" -> patient3.updateContactNumber(userInput.updateContactNumber)
+                    else -> println("Invalid Input")
+                }
+            }
+
+            4 -> {
+                println("Thank you for using the ATM!")
+                break
+            }
+
+            else -> {
+                println("Invalid Input")
+            }
+        }
+        println()
+    }
 
 }
